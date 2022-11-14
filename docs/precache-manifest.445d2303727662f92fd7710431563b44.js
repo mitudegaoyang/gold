2f3b6684aa75187d7ff4c5c434f4c7a8a34d8422
Tianyang Gao <gaotianyang@palline.cn>
2022-11-14 09:17:19 +0800

    feat: 初始化测试

---
+++ docs/precache-manifest.445d2303727662f92fd7710431563b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce534eb632e0237d1a4619bce6f136de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f81b39c01d52d626082d",
+    "url": "/static/css/main.08c85a02.chunk.css"
+  },
+  {
+    "revision": "c1b76e9fd52442e287e9",
+    "url": "/static/js/2.f52550a9.chunk.js"
+  },
+  {
+    "revision": "a4a7a4b2ed253cec296166cf3f6a0eda",
+    "url": "/static/js/2.f52550a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81b39c01d52d626082d",
+    "url": "/static/js/main.10e8f911.chunk.js"
+  },
+  {
+    "revision": "f21368a737d9433732d4",
+    "url": "/static/js/runtime-main.2f3006e3.js"
+  }
+]);
